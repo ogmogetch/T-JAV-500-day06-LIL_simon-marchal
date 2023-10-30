03a6b6d618da51d85f3b4672a2906367a632493c
Simon Marchal <144667975+ogmogetch@users.noreply.github.com>
2023-10-30 23:54:25 +0100

    Update Mage.java

--- Mage.java
+++ Mage.java
@@ -1,40 +1,35 @@
-public class Mage extends Character{
+public class Mage extends Character implements Movable {
     public Mage(String name) {
         super(name, "Mage");
-        int life = 70;
-        int agility = 10;
-        int strength = 3;
-        int wit = 10;
-        System.out.println(name + ": May the gods be with me.");
+        this.life = 70;
+        this.agility = 10;
+        this.strength = 3;
+        this.wit = 10;
+        System.out.println(this.name + ": May the gods be with me.");
     }
 
     @Override
     public void attack(String weapon) {
-        if (weapon.equals("magic") || weapon.equals("wand")) {
-            System.out.println(name + ": Rrrrrrrrr....");
-            System.out.println(name + ": Feel the power of my " + weapon + "!");
-        } else {
-            System.out.println(name + ": I don't need this stupid " + weapon + "! Don't misjudge my powers!");
+        if (weapon == "magic" || weapon == "wand") {
+            System.out.println(this.name + ": Rrrrrrrrr....");
+            System.out.println(this.name + ": Feel the power of my " + weapon + "!");
         }
     }
 
     @Override
     public void moveRight() {
-        System.out.println(name + ": moves right furtively.");
+        System.out.println(this.name + ": moves right furtively.");
     }
 
-    @Override
     public void moveLeft() {
-        System.out.println(name + ": moves left furtively.");
+        System.out.println(this.name + ": moves left furtively.");
     }
 
-    @Override
     public void moveForward() {
-        System.out.println(name + ": moves forward furtively.");
+        System.out.println(this.name + ": moves forward furtively.");
     }
 
-    @Override
     public void moveBack() {
-        System.out.println(name + ": moves back furtively.");
+        System.out.println(this.name + ": moves back furtively.");
     }
 }
